5ddacb93fbc725282c5d5c99a86b62a05f90f8c1
Stephane Raux <94983192+stefunctional@users.noreply.github.com>
2023-05-09 09:20:56 -0500

    Support pairs and arrays (#51)
    * Support returning pairs
    
    * Support arrays

---
+++ safer-ffi-gen/tests/tuple.rs
@@ -0,0 +1,26 @@
+use safer_ffi_gen::{ffi_type, safer_ffi_gen};
+
+#[ffi_type(opaque)]
+pub struct Foo {
+    i: i32,
+}
+
+#[safer_ffi_gen]
+impl Foo {
+    #[safer_ffi_gen]
+    pub fn new_pair(i: i32, j: i32) -> (Foo, Foo) {
+        (Self { i }, Self { i: j })
+    }
+
+    #[safer_ffi_gen]
+    fn get(&self) -> i32 {
+        self.i
+    }
+}
+
+#[test]
+fn pair_can_be_returned() {
+    let pair = foo_new_pair(33, 34);
+    assert_eq!(foo_get(&pair._0), 33);
+    assert_eq!(foo_get(&pair._1), 34);
+}
