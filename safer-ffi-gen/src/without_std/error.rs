ef2d02cd4c0de9f1315947b71a5ea4b12734916f
Stephane Raux <sraux@amazon.com>
2023-05-31 17:36:10 -0500

    Store last error in no_std
    This assumes a single thread

--- safer-ffi-gen/src/without_std/error.rs
+++ safer-ffi-gen/src/without_std/error.rs
@@ -1,12 +1,17 @@
 use alloc::string::{String, ToString};
 
-pub fn set_last_error<E>(_: E)
+static mut LAST_ERROR: String = String::new();
+
+pub fn set_last_error<E>(e: E)
 where
     E: ToString,
 {
+    unsafe {
+        LAST_ERROR = e.to_string();
+    }
 }
 
 #[safer_ffi::ffi_export]
 pub fn last_error() -> safer_ffi::String {
-    String::new().into()
+    unsafe { LAST_ERROR.clone().into() }
 }
