474b6de0e5be0a77a2ad13ee52d341a80a61e07e
Stephane Raux <94983192+stefunctional@users.noreply.github.com>
2023-07-13 15:19:19 -0500

    Replace type parameters with arguments after alias substitution (#65)
    Alias substitution ignores the original type parameters, so this would break if they had already been replaced.

--- safer-ffi-gen/tests/generic.rs
+++ safer-ffi-gen/tests/generic.rs
@@ -17,9 +17,13 @@ impl<T> Foo<T> {
 }
 
 safer_ffi_gen::specialize! { FooI32 = Foo<i32> }
+safer_ffi_gen::specialize! { FooI64 = Foo<i64> }
 
 #[test]
 fn specialization_works() {
     let x = foo_i32_new(33);
     assert_eq!(*foo_i32_get(&x), 33);
+
+    let x = foo_i64_new(33);
+    assert_eq!(*foo_i64_get(&x), 33);
 }
